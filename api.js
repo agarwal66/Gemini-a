f1432e92e479efd73bf8ecd58d8de7d6c6ea72e4
Prateek Agarwal <114166286+agarwal66@users.noreply.github.com>
2025-06-29 19:12:12 +0530

    Update api.js

--- api.js
+++ api.js
@@ -34,9 +34,9 @@ app.post("/generate-text", async (req, res) => {
   try {
     const prompt = req.body.prompt || "Write a story about a magic backpack.";
 
-    // const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+    const model = genAI.getGenerativeModel({ model: "gemini-pro" });
     // const model = genAI.getGenerativeModel({ model: "models/gemini-pro" });
-  const model = genAI.getGenerativeModel({ model: "models/text-bison-001" });
+  // const model = genAI.getGenerativeModel({ model: "models/text-bison-001" });
 
     const result = await model.generateContent(prompt);
     const response = await result.response;
