7303c4d0932017a09cc76d69261c863974b14e18
Prateek Agarwal <114166286+agarwal66@users.noreply.github.com>
2026-01-28 12:50:34 +0530

    Update api.js

--- api.js
+++ api.js
@@ -3,7 +3,6 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const axios = require("axios");
 require("dotenv").config();
-
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
